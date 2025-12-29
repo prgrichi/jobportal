0ca200cef33509543838649ffd8c2912d40a87fd
Richard Holzner <richardholzner@Richards-MacBook-Pro.local>
2025-12-29 18:09:38 +0100

    Add favoriteJobs page

---
+++ src/pages/FavoriteJobsPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="py-6 md:py-12">
+    <div class="max-w-app mx-auto px-4">
+      <h2 class="text-2xl font-bold text-neutral-900 mb-4">Meine Jobs</h2>
+      <p>Du hast noch keine gespeicherten Jobs</p>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'FavoriteJobsPage'
+}
+</script>
+
+<style scoped></style>
