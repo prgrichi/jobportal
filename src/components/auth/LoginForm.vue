0831f3658fb71532f806b79031003f490d19d188
Richard Holzner <richard.holzner@gmail.com>
2026-01-13 15:45:54 +0100

    Add translations for demo login hint

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,10 +1,10 @@
 <template>
   <AuthForm mode="login" />
   <div v-if="showDemoCredentials" class="mt-6 rounded-xl bg-blue-50 p-4 text-sm text-blue-900">
-    <p class="font-medium">Demo-Zugang verfügbar</p>
+    <p class="font-medium">{{ $t('demo.title') }}</p>
     <p class="mt-1 font-mono">demo@jobportal.dev</p>
     <p class="mt-1 font-mono">demo123</p>
-    <p class="mt-1 text-blue-800">Alternativ ist eine freie Registrierung möglich.</p>
+    <p class="mt-1 text-blue-800">{{ $t('demo.alternative') }}</p>
   </div>
 </template>
 
